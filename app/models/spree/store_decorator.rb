47c8c62e963bace24f0930d50addf617ec530aa8
Rudi <rudi@rudikhoury.com>
2026-01-13 13:48:53 +1000

    Remove store_decorator to avoid conflicts with host app decorators

--- app/models/spree/store_decorator.rb
+++
@@ -1,9 +0,0 @@
-module Spree
-    module StoreDecorator
-      def self.prepended(base)
-        base.has_one :google_credential, class_name: 'Spree::GoogleCredential', dependent: :destroy
-      end
-    end
-  end
-  
-  ::Spree::Store.prepend(Spree::StoreDecorator)
